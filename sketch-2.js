03dd20d20671b6c80aacae97dd615cda696cd598
Lorenzo Bernini <lorenzobernini98@gmail.com>
2022-03-31 19:50:39 +0200

    nuove copertine

--- sketch-2.js
+++ sketch-2.js
@@ -4,17 +4,17 @@ let scientifico;
 let umanistico;
 
 function preload() {
-  s1 = loadImage("assets/copertine/small/S1.png");
-  s2 = loadImage("assets/copertine/small/S2.png");
-  s3 = loadImage("assets/copertine/small/S3.png");
-  s4 = loadImage("assets/copertine/small/S4.png");
-  s5 = loadImage("assets/copertine/small/S5.png");
+  s1 = loadImage("assets/copertine/nuove/low-res/S1.png");
+  s2 = loadImage("assets/copertine/nuove/low-res/S2.png");
+  s3 = loadImage("assets/copertine/nuove/low-res/S3.png");
+  s4 = loadImage("assets/copertine/nuove/low-res/S4.png");
+  s5 = loadImage("assets/copertine/nuove/low-res/S5.png");
 
-  u1 = loadImage("assets/copertine/small/U1.png");
-  u2 = loadImage("assets/copertine/small/U2.png");
-  u3 = loadImage("assets/copertine/small/U3.png");
-  u4 = loadImage("assets/copertine/small/U4.png");
-  u5 = loadImage("assets/copertine/small/U5.png");
+  u1 = loadImage("assets/copertine/nuove/low-res/U1.png");
+  u2 = loadImage("assets/copertine/nuove/low-res/U2.png");
+  u3 = loadImage("assets/copertine/nuove/low-res/U3.png");
+  u4 = loadImage("assets/copertine/nuove/low-res/U4.png");
+  u5 = loadImage("assets/copertine/nuove/low-res/U5.png");
 
   scientifico = [s1, s2, s3, s4, s5];
   umanistico = [u1, u2, u3, u4, u5];
@@ -46,8 +46,8 @@ function draw() {
     scientifico[randomS],
     0,
     0,
-    scientifico[randomS].width / 2,
-    scientifico[randomS].height / 2
+    scientifico[randomS].width / 1.6,
+    scientifico[randomS].height / 1.6
   );
   pop();
 
@@ -55,7 +55,13 @@ function draw() {
   translate(width / 2, height / 2);
   rotate(random(0, 350));
   translate(randomXU, randomYU);
-  image(umanistico[randomU], 0, 0, umanistico[randomU].width / 2, umanistico[randomU].height / 2);
+  image(
+    umanistico[randomU],
+    0,
+    0,
+    umanistico[randomU].width / 1.6,
+    umanistico[randomU].height / 1.6
+  );
   pop();
 }
 
