200c192b6b1ee9f7cedf73e316b2148580c4ff3d
Lorenzo Bernini <lorenzobernini98@gmail.com>
2024-10-08 10:31:51 +0200

    mobile again

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//--------APPUNTI DROPDOWN
+//-------- APPUNTI DROPDOWN - DESKTOP
 
 // Get current page URL to determine active selection
 document.addEventListener("DOMContentLoaded", function () {
@@ -31,6 +31,43 @@ function updatePage() {
 // Update page when a year is clicked
 function navigateToPage(year) {
   const indirizzo = document.getElementById("indirizzoSelect").value;
+  const newPage = "appunti-" + indirizzo + "-" + year + ".html";
+  window.location.href = newPage;
+}
+
+//-------- APPUNTI DROPDOWN - MOBILE
+
+// Get current page URL to determine active selection
+document.addEventListener("DOMContentLoaded", function () {
+  const indirizzoSelect = document.getElementById("navSelect-2");
+  const currentPage = window.location.href.split("-");
+  const currentIndirizzo = currentPage[1];
+  const currentYear = currentPage[2].charAt(0);
+
+  // Set the selected indirizzo and highlight the active year button
+  indirizzoSelect.value = currentIndirizzo;
+  const activeButton = document.getElementById("yearMob" + currentYear);
+  activeButton.classList.add("attivo");
+
+  // Set the background color based on indirizzo
+  if (currentIndirizzo === "S") {
+    activeButton.classList.add("scientifico");
+  } else if (currentIndirizzo === "U") {
+    activeButton.classList.add("umanistico");
+  }
+});
+
+// Update page when indirizzo (school type) is changed
+function updatePage() {
+  const indirizzo = document.getElementById("navSelect-2").value;
+  const activeYear = document.querySelector(".years button.attivo").innerText;
+  const newPage = "appunti-" + indirizzo + "-" + activeYear + ".html";
+  window.location.href = newPage;
+}
+
+// Update page when a year is clicked
+function navigateToPage(year) {
+  const indirizzo = document.getElementById("navSelect-2").value;
   const newPage = "appunti-" + indirizzo + "-" + year + ".html";
   window.location.href = newPage;
 }
